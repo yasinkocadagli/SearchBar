e7e1a019b879d6033f5793f34103fc79713f8af2
yasin <yasinkocadagli@gmail.com>
2023-03-12 17:13:41 +0300

    search bar settings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import { SearchBar } from './components/SearchBar';
 
 function App() {
- 
+ return(
+  <div className="App">
+    <div className="search-bar-container">
+      <SearchBar/>
+      <div >SearchResults</div>
+    </div>
+  </div>
+ )
 }
 
 export default App;
